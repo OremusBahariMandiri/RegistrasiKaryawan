ba201405e47deab660a5c8ccb6b00e2a50371ffc
Jamas108 <oremus.itdept@gmail.com>
2025-10-08 08:31:30 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -5,10 +6,23 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            // host:'192.168.0.39' // host JAMAS
+            host: '192.168.1.17' // host OREMUS
+        },
+        cors: {
+            // origin: ['http://192.168.0.39:8080', 'http://127.0.0.1:8000'], // Server JAMAS
+            origin: ['http://192.168.1.17:8082', 'http://127.0.0.1:8000'], // SERVER OREMUS
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            credentials: true
+        }
+    },
 });
